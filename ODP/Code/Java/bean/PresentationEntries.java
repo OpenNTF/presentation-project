8b9124fe078a0bcbda769a396d80720fa8434def
Oliver Busse <obusse@googlemail.com>
2022-12-07 19:52:36 +0100

    fixed stuff

--- ODP/Code/Java/bean/PresentationEntries.java
+++ ODP/Code/Java/bean/PresentationEntries.java
@@ -1,8 +1,49 @@
 package bean;
 
+import java.time.LocalDate;
+import java.time.temporal.ChronoField;
+import java.util.Comparator;
+import java.util.List;
+import java.util.SortedMap;
+import java.util.SortedSet;
+import java.util.TreeMap;
+import java.util.TreeSet;
+import java.util.stream.Collectors;
+
+import com.ibm.commons.util.StringUtil;
+
 import jakarta.enterprise.context.RequestScoped;
+import jakarta.inject.Inject;
+import jakarta.nosql.mapping.Pagination;
+import jakarta.ws.rs.NotFoundException;
+import model.presentation.PresentationEntry;
 
 @RequestScoped
 public class PresentationEntries {
+	
+	@Inject
+	PresentationEntry.Repository presentationEntryRepository;
+	
+	public SortedMap<LocalDate, SortedSet<PresentationEntry>> getEntrySummaries() {
+		SortedMap<LocalDate, SortedSet<PresentationEntry>> result = new TreeMap<>(Comparator.reverseOrder());
 
+		presentationEntryRepository.findAll()
+			.filter(entry -> StringUtil.isNotEmpty(entry.getUnid()))
+			.forEach(entry -> {
+				LocalDate midMonth = entry.getOffsetDateTime().toLocalDate().with(ChronoField.DAY_OF_MONTH, 15);
+				result.computeIfAbsent(midMonth, d -> new TreeSet<>(Comparator.reverseOrder())).add(entry);
+			});
+
+		return result;
+	}
+	
+	public PresentationEntry getEntry(String unid) {
+		return presentationEntryRepository.findById(unid)
+			.orElseThrow(() -> new NotFoundException("Unable to find blog entry for ID: " + unid));
+	}
+
+	public List<PresentationEntry> getEntries(int limit) {
+		return presentationEntryRepository.findRecent(Pagination.page(1).size(limit))
+			.collect(Collectors.toList());
+	}
 }
--- ODP/Code/Java/webapp/controller/PresentationsController.java
+++ ODP/Code/Java/webapp/controller/PresentationsController.java
@@ -1,9 +1,14 @@
 package webapp.controller;
 
+import bean.PresentationEntries;
 import jakarta.inject.Inject;
 import jakarta.mvc.Controller;
 import jakarta.mvc.Models;
+import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
+import jakarta.ws.rs.PathParam;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
 
 @Path("/")
 @Controller
@@ -12,5 +17,22 @@ public class PresentationsController {
 	Models models;
 	
 	@Inject
-	BlogEntries blogEntries;
+	PresentationEntries presentationEntries;
+	
+	@GET
+	@Produces(MediaType.TEXT_HTML)
+	public String get() {
+		models.put("blogEntrySummaries", presentationEntries.getEntrySummaries()); //$NON-NLS-1$
+		
+		return "blog.jsp"; //$NON-NLS-1$
+	}
+	
+	@Path("{entryUnid}")
+	@GET
+	@Produces(MediaType.TEXT_HTML)
+	public String getEntry(@PathParam("entryUnid") String entryUnid) {
+		models.put("entry", presentationEntries.getEntry(entryUnid));
+		
+		return "blogEntry.jsp";
+	}
 }
